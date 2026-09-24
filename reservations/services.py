e280d9569b6d1e778b0f3d77a74761260427ea65
rezagh100 <gholamhasanireza@gmail.com>
2026-09-24 23:24:56 +0330

    fix some bugs

--- reservations/services.py
+++ reservations/services.py
@@ -1,18 +1,11 @@
 from rest_framework.serializers import ValidationError
-
-
        
 class ReservationService():
     
-    def check_seat(self,):
-            
-        
-        
-        
-class CheckSeatAndHall():
-    def check_seat_with_hall(self,showtime,seat):
+    def check_seat_hall(self,showtime,seat):
         if showtime.hall != seat.hall:
             raise ValidationError({'message':'این صندلی متعلق به سالن این سانس نیست'})
         
- 
-    
+    def reserv_seat(self,seat,showtime):
+        self.check_seat_hall(showtime,seat)
+        
